cbd87604158b78e7bbf4f59e7bc161cb867f0418
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-04 15:34:14 +0200

    1_WorkingWithLists

---
+++ 1_WorkingWithLists/theory.py
@@ -0,0 +1,52 @@
+"""
+##################################   Python List Methods    #################################################
+# .count()              a list method to count the number off occurences of an element in a list            #
+# .insert()             a list method to insert an element into a specific index of a list                  #
+# .pop()                a list method to remove an element from a spacific index or from the end of a list  #
+# range()               a built-in method Python function to create a sequence of integers                  #
+# len()                 a built-in method Python function to get the lengh of a list                        #
+# .sort() / sorted()    a method and a built-in function to sort a list                                     #
+#############################################################################################################
+
+
+Working with Lists in Python
+Working with Lists
+
+Now that we know how to create and access list data, we can start to explore additional ways of working with lists.
+
+In this lesson, you’ll learn how to:
+
+    Add and remove items from a list using a specific index.
+    Create lists with continuous values.
+    Get the length of a list.
+    Select portions of a list (called slicing).
+    Count the number of times that an element appears in a list.
+    Sort a list of items.
+
+Note: In some of the exercises, we will be using built-in functions in Python. If you haven’t yet explored the concept of a function, it may look a bit new. Below we compare it to the method syntax we learned in the earlier lesson.
+
+Here is a preview:
+
+# Example syntax for methods
+list.method(input)
+
+# Example syntax for a built-in function
+builtinfuncion(input)
+
+Instructions
+
+Take a second to preview some of the things we will be learning by examining the graphic of common list methods and built-in functions.
+
+When you’re ready, continue to the next exercise.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    Is it possible to sort() a list but do it in reverse order?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+
+##################################################################
+"""
