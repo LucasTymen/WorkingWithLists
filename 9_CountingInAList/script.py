b053db73a37335770b081fa407869f217ff25cfa
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-06 01:49:26 +0200

    9_CountingInAList

--- 9_CountingInAList/script.py
+++ 9_CountingInAList/script.py
@@ -1,3 +1,12 @@
 votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
 
 # Your code below:
+jake_votes = votes.count("Jake")
+print(jake_votes)
+
+# extra practicing
+letters = ["m", "i", "s", "s", "i", "s", "s", "i", "p", "p", "i"]
+num_i = letters.count("i")
+print(num_i)
+num_s = letters.count("s")
+print(num_s)
