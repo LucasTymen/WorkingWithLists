8cf1b1199e69562d1b22cc2e88977755fa8c3fcd
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-06 00:25:26 +0200

    8_SlicingLists_2

---
+++ 8_SlicingLists_2/script.py
@@ -0,0 +1,11 @@
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+# Your code below:
+
+# step 1
+last_two_elements = suitcase[-2:]
+print(last_two_elements)
+
+# step 2
+slice_off_last_three = suitcase[:-3]
+print(slice_off_last_three)
