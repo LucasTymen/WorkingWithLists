85f892172eabda2496191051d13dd8c1699ae5bf
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-05 17:08:25 +0200

    6_Len_length

--- 6_Len_length/flles/script.py
+++ 6_Len_length/flles/script.py
@@ -1,5 +1,16 @@
 long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
 
-big_range = range(2, 3000, 10)
+big_range = range(2, 3000, 100) #Changed 10 for 100
 
 # Your code below:
+# step 1
+long_list_len = len(long_list)
+
+# step 2
+print(long_list_len)
+
+# step 3
+big_range_length = len(big_range)
+
+# step 4
+print(big_range_length)
