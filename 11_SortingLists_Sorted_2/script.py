d5f94fe939c6df5bea46820d3609a3f40a84c77e
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-06 03:26:44 +0200

    11_SortingLists_Sorted_2

--- 11_SortingLists_Sorted_2/script.py
+++ 11_SortingLists_Sorted_2/script.py
@@ -1,3 +1,8 @@
 games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
 
 # Your code below:
+games_sorted = sorted(games)
+print(games)
+# prints :['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
+print(games_sorted)
+#prints : ['Minecraft', 'Pacman', 'Pokemon', 'Portal', 'Tetris', 'The Sims']
